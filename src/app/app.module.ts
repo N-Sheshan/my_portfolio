9c31df55b33ed2a9f4b8e4b7648db696e3411712
unknown <pavotigris9218@gmail.com>
2023-04-20 23:11:55 +0530

    navbar have changed in hostel day1(20/4/23)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,MatToolbarModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
